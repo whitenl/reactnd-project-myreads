e0a286f68a55dc313e8efaab55d1eaa35903df03
Nic White <nicole.l.white@gmail.com>
2018-11-04 14:10:52 -0800

    fixed selected value in dropdown

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Shelf from './Shelf.js' 
 
 class Book extends Component {
-	render() {
-		let displayThumbnail = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : '';
-		return (
-			<div className="book">
+    render() {
+        let book = this.props.book
+        let displayThumbnail = book.imageLinks ? book.imageLinks.thumbnail : '';
+        
+        return (
+            <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ 
                         width: 128, 
@@ -12,24 +15,26 @@ class Book extends Component {
                         backgroundImage: `url("${displayThumbnail}")` }}>
                     </div>
                         <div className="book-shelf-changer">
-                            <select
-                            	onChange={(event) => this.props.moveShelf(this.props.book, event.target.value
-                            	)} 
-                            	value={this.props.currentShelf}				
-                            >
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
+                            <select value={book.shelf || this.props.currentShelf}  
+                                onChange={(e) => this.props.moveShelf(book, e.target.value)}>
+                            <option value="move" disabled>Move to...</option>
+
+                            {
+                                Shelf.shelves.map((shelf) => (
+                                    <option key={shelf.id} value={shelf.id}>{shelf.title}</option>
+                                    ))
+
+                            }
+                               
+                            <option value="none">None</option>
                             </select>
                         </div>
                     </div>
                 <div className="book-title">{this.props.book.title}</div>
                 <div className="book-authors">{this.props.book.authors}</div>
             </div>
-		);
-	} 
+        );
+    } 
 
 }
 
