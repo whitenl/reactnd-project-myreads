bfe22121bfbdc448fafe2e33adf5b94a0cd7dd4e
Nic White <nicole.l.white@gmail.com>
2018-11-05 21:34:38 -0800

    separated multiple authors with commas

--- src/components/Book.js
+++ src/components/Book.js
@@ -37,7 +37,7 @@ class Book extends Component {
           </div>
         </div>
         <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors}</div>
+        <div className="book-authors">{this.props.book.authors ? this.props.book.authors.join(', ') : ''}</div>  
       </div>
     );
   }
